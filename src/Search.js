c5093d7fc540f432dd80508b820511f2912ec41f
Inthenew <38168276+Inthenew@users.noreply.github.com>
2021-11-15 02:30:38 +0000

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -43,7 +43,7 @@ window.getResults = function () {
         search: search
     }).then(function (res) {
         for (let i = 0; i < res.data.length; i++) {
-            document.getElementById('reslts').innerHTML += `<div style="border-style: solid;  display: inline-block;  width: -moz-fit-content; width: fit-content; border-color: black;" ><a href="https://server234.glitch.me/${res.data[i].id}">${res.data[i].title}</h4><h6>Made by: ${res.data[i].user}</h6></a> </div>
+            document.getElementById('reslts').innerHTML += `<div style="border-style: solid;  display: inline-block;  width: -moz-fit-content; width: fit-content; border-color: black;" ><a href="https://server234.glitch.me/games/${res.data[i].id}">${res.data[i].title}</h4><h6>Made by: ${res.data[i].user}</h6></a> </div>
             `;
         }
         if (res.data.length <= 0) {
